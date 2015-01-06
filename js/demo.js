2112e92c403e87593d71e4546de809aa2f546dde
capnramses <lists@antongerdelan.net>
2015-01-06 15:55:52 +0000

    fleshy skeleton

--- js/demo.js
+++ js/demo.js
@@ -1,1 +1,40 @@
-this will be a demo
+//
+// Example WebGL programme to test text rendering
+// Anton Gerdelan, 6 Jan 2015
+
+var canvas;
+var gl;
+var gl_width;
+var gl_height;
+
+main ();
+
+function main () {
+	canvas = document.getElementById ("canvas");
+	gl = WebGLUtils.setupWebGL (canvas);
+	canvas_resize ();
+	gl.clearColor (0.2, 0.2, 0.2, 1.0);
+	gl.enable (gl.DEPTH_TEST);
+	gl.cullFace (gl.BACK);
+	gl.frontFace (gl.CCW);
+	gl.enable (gl.CULL_FACE);
+	gl.blendFunc (gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+	update ();
+}
+
+function canvas_resize () {
+	gl_width = canvas.clientWidth;
+	gl_height = canvas.clientHeight;
+	gl.viewport (0, 0, gl_width, gl_height);
+	console.log ("client canvas size: " + gl_width + "x" + gl_height);
+}
+
+
+function update () {
+	draw ();
+	window.requestAnimFrame (update, canvas);
+}
+
+function draw () {
+	gl.clear (gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+}
